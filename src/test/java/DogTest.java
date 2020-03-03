4d8d38dfd452b563798aa5abbf52df5c6e830fa7
Matthew Chen <hao-kun.chen@microfocus.com>
2020-03-03 20:52:27 +0800

    create a junit/surefire demo project

---
+++ src/test/java/DogTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DogTest {
+    @Test
+    public void RedDogTest(){
+        System.out.println("RedHatTest");
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void BlueDogTest(){
+        System.out.println("RedHatTest");
+        assertEquals(2, 2);
+    }
+}
---
+++ src/test/java/HatTest2.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HatTest2 {
+    @Test
+    public void RedHatTest(){
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void BlueHatTest(){
+        assertEquals(2, 2);
+    }
+}
