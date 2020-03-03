c457ee5209c7a1b3becf2c3c755bbceb5e691c4d
Matthew Chen <hao-kun.chen@microfocus.com>
2020-03-03 21:00:02 +0800

    modify test

--- src/test/java/HatTest.java
+++ src/test/java/HatTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class HatTest2 {
+public class HatTest {
     @Test
     public void RedHatTest(){
         assertEquals(2, 2);
